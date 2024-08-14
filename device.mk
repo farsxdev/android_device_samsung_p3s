7753cc0b1c8cdc82be1c271f2bebd8d0be56b689
Fars <101596708+farsxdev@users.noreply.github.com>
2024-08-14 12:06:15 +0300

    p3s: Disable ART debug and optimize dexpreopt
     * don't include debug configs in art
     * use everything filter for dexpreopt (AOSPA/android_vendor_pa@ab5061c)
     * use non-debug dex2oat
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Co-Authored-By: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -30,6 +30,11 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
+# Dex/ART optimization
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+USE_DEX2OAT_DEBUG := false
+
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
 
